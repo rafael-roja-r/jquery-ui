585119731e42a2bffa2c01108e574a358e2bca09
rafaelrr1 <rafaelrr1@outlook.es>
2019-04-09 22:37:14 -0500

    mi juego de dama clasico echo en jquery-ui

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var Dificultad = 3;
+$(document).ready(function () {
+  var fps = 1000;
+  setInterval(function () {
+    $(".main-titulo").css("color", "White");
+  }, 1000 / 3);
+
+  var fps = 1000;
+  setInterval(function () {
+    $(".main-titulo").css("color", "#040014");
+  }, 4 / 1000);
+
+    
+
+
+    $(document).ready(function () {
+                
+            $(".ficha1").draggable();
+             $(".ficha2").draggable();
+
+            $(".table").droppable({
+            
+            });
+          });
+  
+
+
+});
